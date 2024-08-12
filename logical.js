f3a42e604b5a9647d4c1c13da964ef807c7cc1d8
HARITHAHARRY <harithakkatholi@gmail.com>
2024-08-12 12:54:22 +0530

    first commit

---
+++ logical.js
@@ -0,0 +1,32 @@
+
+// const arr=[1,2,3,4,5,6]
+// let newarr=[]
+// for(i=0;i<arr.length;i++)
+// {
+//     if (i%2==0)
+// {
+
+// newarr.push(arr[i])
+// }
+// }
+// console.log(newarr);
+
+// const arr=[10,20,30,40,50]
+// let newarr=[]
+// for(i=0;i<arr.length;i++)
+// {
+//     if (i%2==0)
+// {
+
+// newarr.push(arr[i])
+// }
+// }
+// console.log(newarr);
+
+    // const str="hello world";
+    // const newsr=str.split("o").join("")
+    // console.log(newsr);
+
+    const str="phython programming";
+    const newsr=str.split("p").join("")
+    console.log(newsr);
